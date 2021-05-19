5b58b89d8f79f939fefed79678c64e006e2e2394
Eugene Kravec <krava255@gmail.com>
2021-05-20 00:24:46 +0300

    Grids Components

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://thumb.tildacdn.com/tild6331-6165-4633-a433-626163313532/-/format/webp/PAGL-logo.png"
+        alt="logo__pagl"
+      />
+    </header>
+  );
+};
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,20 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://static.tildacdn.com/tild3239-6363-4036-a137-353162636132/ezgifcom-video-to-gi.gif"
+          alt="main_img"
+        />
+      </div>
+      <div>ava + discription</div>
+      <div>
+        my posts
+        <div>new post</div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
+  );
+};
+export default Profile;
