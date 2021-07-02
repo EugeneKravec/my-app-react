fd436f8913a401b1bf91256e1e807f4768966cc5
Eugene Kravec <krava255@gmail.com>
2021-07-02 09:51:10 +0300

    Added Route path

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
-// import Profile from "./Components/Profile/Profile";
+import Profile from "./Components/Profile/Profile";
 // import MyPosts from "./Components/Profile/MyPosts/MyPosts";
 import Dialogs from "./Components/Dialogs/Dialogs";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="wrapper">
-      <Header />
-      <Navbar />
-      <div class="wrapper-content">
-        <Dialogs />
+    <BrowserRouter>
+      <div className="wrapper">
+        <Header />
+        <Navbar />
+          <div class="wrapper-content">
+            <Route path = '/dialogs' component={Dialogs} />
+            <Route path = '/profile' component={Profile} />
+            <Route path = '/news' component={News} />
+            <Route path = '/music' component={Music} />
+            <Route path = '/settings' component={Settings} />
+          </div>
       </div>
-      {/* <Profile /> */}
-    </div>
+    </BrowserRouter>
   );
 }
 
