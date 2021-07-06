e5111e4608ba184072f39b7c3c562b694387856f
Eugene Kravec <krava255@gmail.com>
2021-07-06 14:04:06 +0300

    added active link color

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,6 +1,6 @@
 // import s from './Dialogs.module.css';
 
-const Dialogs = (props) => {
+const Dialogs = () => {
     return (
         <div>
             Dialogs
--- src/Components/News/News.jsx
+++ src/Components/News/News.jsx
@@ -1,10 +1,12 @@
 // import s from './News.module.css';
 
-const News = (props) => {
-    return ( <div>
-        News
-    </div>
-    )
-}
+
+const News = () => {
+  return (
+  <div>
+      News
+      </div>
+  );
+};
 
 export default News;
