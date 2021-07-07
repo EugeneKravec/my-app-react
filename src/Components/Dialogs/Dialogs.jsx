96dba0a14420721ceb59081184aa5403c15f4135
Eugene Kravec <krava255@gmail.com>
2021-07-07 17:26:12 +0300

    Added dialogs

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,11 +1,23 @@
-// import s from './Dialogs.module.css';
+import s from "./Dialogs.module.css";
 
-const Dialogs = () => {
-    return (
-        <div>
-            Dialogs
+const Dialogs = (props) => {
+  return (
+    <div className = {s.dialogs}>
+        <div className = {s.dialogsItems}>
+            <div className = {s.active}>Eugene</div>
+            <div className = {s.dialog}>Sasha</div>
+            <div className = {s.dialog}>Elena</div>
+            <div className = {s.dialog}>Luda</div>
+            <div className = {s.dialog}>Tanya</div>
+            <div className = {s.dialog}>Misha</div>
         </div>
-    )
-}
+        <div className = {s.messages}>
+            <div className={s.message}>Hi</div>
+            <div className={s.message}>How are your IT?</div>
+            <div className={s.message}>YO</div>
+        </div>
+    </div>
+  );
+};
 
 export default Dialogs;
