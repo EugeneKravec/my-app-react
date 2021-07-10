4a99ff02a76f6bf62bf046ce0e135e0809d66470
Eugene Kravec <krava255@gmail.com>
2021-07-10 15:39:10 +0300

    start making posts

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,16 +1,11 @@
-import s from "./Profile.module.css";
+// import s from "./Profile.module.css";
 import MyPosts from "./MyPosts/MyPosts";
+import ProfileInfo from "./Profileinfo/Profileinfo";
 
 const Profile = () => {
   return (
-    <div className={s.content}>
-      <div>
-        <img
-          src="https://static.tildacdn.com/tild3239-6363-4036-a137-353162636132/ezgifcom-video-to-gi.gif"
-          alt="main_img"
-        />{" "}
-      </div>
-      <div>ava + discription</div>
+    <div>
+      <ProfileInfo />
       <MyPosts />
     </div>
   );
---
+++ src/Components/Profile/Profileinfo/Profileinfo.jsx
@@ -0,0 +1,12 @@
+import s from "./Profileinfo.module.css";
+
+const ProfileInfo = () => {
+  return (<div>
+    <img
+          src="https://static.tildacdn.com/tild3239-6363-4036-a137-353162636132/ezgifcom-video-to-gi.gif"
+          alt="main_img"
+        />
+         <div className={s.descriptionBlock}>ava + discription</div>
+  </div>);
+};
+export default ProfileInfo;
